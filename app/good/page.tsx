48ea7fd4b43ee2588af82786d3748465dd261fb7
prashantttzz <contact.prashant31@gmail.com>
2026-01-05 19:23:15 +0530

    new branch commit

---
+++ app/good/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Good = () => {
+  return (
+    <div>testing for codefox automatic ai review</div>
+  )
+}
+
+export default Good
--- app/testing/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Page = () => {
-  return <div>testing for code fox </div>;
-};
-
-export default Page;
